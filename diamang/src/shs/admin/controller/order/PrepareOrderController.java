e0c0f4cf058a0be21fcd86df815d44ff8364270c
zazuuka <zazuuka@gmail.com>
2018-02-21 18:42:52 +0900

    prepareproduct.jsp에 페이징 적용. dao 자잘한 수정.

--- diamang/src/shs/admin/controller/order/PrepareOrderController.java
+++ diamang/src/shs/admin/controller/order/PrepareOrderController.java
@@ -1,7 +1,6 @@
 package shs.admin.controller.order;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -11,8 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import shs.admin.dao.order.PrepareproductDao;
+import shs.admin.etc.PagingBot;
 import shs.admin.vo.order.PrepProdVo;
 import shs.admin.vo.order.SearchOptionVo;
+import shs.admin.vo.paging.PagingVo;
 
 @WebServlet("/admin/prepareProduct.do")
 public class PrepareOrderController extends HttpServlet{
@@ -31,11 +32,14 @@ public class PrepareOrderController extends HttpServlet{
 		String optName = req.getParameter("optName");
 		String optValue = req.getParameter("optValue");
 		String buy_date = req.getParameter("buy_date");
-		String item_name = req.getParameter("item_name");
-		
+		String item_name = req.getParameter("item_name");	
+		String pageNum = req.getParameter("pageNum");
 		SearchOptionVo vo = new SearchOptionVo(optName, optValue, buy_date, item_name);		
 		PrepareproductDao dao = new PrepareproductDao();
-		ArrayList<PrepProdVo> list = dao.search(vo);
+		PagingBot bot = new PagingBot(5,10);
+		PagingVo pageVo =  bot.calPaging(pageNum, dao.getCount(vo));
+		ArrayList<PrepProdVo> list = dao.search(vo, pageVo);
+		req.setAttribute("pageVo", pageVo);
 		req.setAttribute("search", vo);
 		req.setAttribute("list", list);
 		req.getRequestDispatcher("/admin/order/prepareproduct.jsp").forward(req, resp);
--- diamang/src/shs/admin/vo/paging/PagingVo.java
+++ diamang/src/shs/admin/vo/paging/PagingVo.java
@@ -1,6 +1,8 @@
 package shs.admin.vo.paging;
 
 public class PagingVo {
+	private int pageRowVolume;
+	private int currentPageVolume;
 	private int pageNum;
 	private int startRow;
 	private int endRow;
@@ -8,8 +10,12 @@ public class PagingVo {
 	private int startPage;
 	private int endPage;
 	
-	public PagingVo() {};
-	public PagingVo(int pageNum, int startRow, int endRow, int pageCount, int startPage, int endPage) {
+	public PagingVo() {}
+
+	public PagingVo(int pageRowVolume, int currentPageVolume, int pageNum, int startRow, int endRow, int pageCount,
+			int startPage, int endPage) {
+		this.pageRowVolume = pageRowVolume;
+		this.currentPageVolume = currentPageVolume;
 		this.pageNum = pageNum;
 		this.startRow = startRow;
 		this.endRow = endRow;
@@ -17,41 +23,68 @@ public class PagingVo {
 		this.startPage = startPage;
 		this.endPage = endPage;
 	}
-	
+
+	public int getPageRowVolume() {
+		return pageRowVolume;
+	}
+
+	public void setPageRowVolume(int pageRowVolume) {
+		this.pageRowVolume = pageRowVolume;
+	}
+
+	public int getCurrentPageVolume() {
+		return currentPageVolume;
+	}
+
+	public void setCurrentPageVolume(int currentPageVolume) {
+		this.currentPageVolume = currentPageVolume;
+	}
+
 	public int getPageNum() {
 		return pageNum;
 	}
+
 	public void setPageNum(int pageNum) {
 		this.pageNum = pageNum;
 	}
+
 	public int getStartRow() {
 		return startRow;
 	}
+
 	public void setStartRow(int startRow) {
 		this.startRow = startRow;
 	}
+
 	public int getEndRow() {
 		return endRow;
 	}
+
 	public void setEndRow(int endRow) {
 		this.endRow = endRow;
 	}
+
 	public int getPageCount() {
 		return pageCount;
 	}
+
 	public void setPageCount(int pageCount) {
 		this.pageCount = pageCount;
 	}
+
 	public int getStartPage() {
 		return startPage;
 	}
+
 	public void setStartPage(int startPage) {
 		this.startPage = startPage;
 	}
+
 	public int getEndPage() {
 		return endPage;
 	}
+
 	public void setEndPage(int endPage) {
 		this.endPage = endPage;
-	}			
+	}	
 }
