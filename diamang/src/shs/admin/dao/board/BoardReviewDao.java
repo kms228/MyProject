ba0b50cdfc8847a19a85480d2dcf0f4f061bd808
zazuuka <zazuuka@gmail.com>
2018-03-05 12:43:03 +0900

    관리자 메뉴 도움말 씀. 게시물 보일수 있게 쿼리 일부 수정.

--- diamang/src/shs/admin/dao/board/BoardReviewDao.java
+++ diamang/src/shs/admin/dao/board/BoardReviewDao.java
@@ -18,7 +18,7 @@ public class BoardReviewDao {
 		ResultSet rs = null;
 		try {
 			con = DbcpBean.getConn();
-			String sql = "SELECT NVL(COUNT(*),0) CNT FROM REVIEW WHERE MNUM != 10000 AND REGDATE >= SYSDATE -5";									
+			String sql = "SELECT NVL(COUNT(*),0) CNT FROM REVIEW WHERE MNUM != 10000 AND REGDATE >= SYSDATE -20";									
 			pstmt = con.prepareStatement(sql);													
 			rs = pstmt.executeQuery();
 			int cnt = -1;
@@ -126,7 +126,7 @@ public class BoardReviewDao {
 					 										"FROM REVIEW R1, REVIEW R2 " + 
 					 										"WHERE R1.RV_NUM = R2.REF AND R2.MNUM = 10000 " + 
 					 										"GROUP BY R1.RV_NUM) R3 " + 
-					 			 "WHERE R.MNUM=M.MNUM AND R.RV_NUM=R3.RV_NUM(+) AND R.MNUM != 10000 AND REGDATE >= SYSDATE -5 " + 
+					 			 "WHERE R.MNUM=M.MNUM AND R.RV_NUM=R3.RV_NUM(+) AND R.MNUM != 10000 AND REGDATE >= SYSDATE -20 " + 
 					 			 "ORDER BY REF DESC, STEP ASC"
 					 		   + ") R4 " + 
 					 	   ") " + 
